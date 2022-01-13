7c60e1f2b936e0aaf2c963300003021e4f6b2b03
Hewan <Darmelowhewan@gmail.com>
2022-01-13 12:31:41 -0500

    dya38:inheritance_Method OverRiding

---
+++ day38_Inheritance/ParkingLoit.java
@@ -0,0 +1,8 @@
+package day38_Inheritance;
+
+public class ParkingLoit {
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("camery",2020,"gray",2000);
+    }
+  //  Tesla tesla = new Tesla("")
+}
